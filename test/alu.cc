34255b4a7b989bb9b3af798a4e6df2f26f9fedff
Ryang Sohn <loop.infinitely@gmail.com>
2024-01-06 13:01:56 +0900

    test(alu): add test cases for unsigned less than

--- test/alu.cc
+++ test/alu.cc
@@ -228,3 +228,20 @@ TEST_F(AluTest, LessThan) {
     EXPECT_EQ(((uint32_t)alu->out) & 1, expected);
   }
 }
+
+TEST_F(AluTest, LessThanUnsigned) {
+  std::vector<std::tuple<std::tuple<uint32_t, uint32_t>, uint32_t>> test_cases =
+      {{{0, 0}, 0},          {{1, 0}, 0},
+       {{0, 1}, 1},          {{1, 1}, 0},
+       {{0x7fffffff, 1}, 0}, {{0x7fffffff, 0x80000000}, 1},
+       {{0x80000000, 1}, 0}, {{0x80000000, 0x80000000}, 0}};
+
+  for (auto &[input, expected] : test_cases) {
+    auto &[in1, in2] = input;
+    alu->op = 9;
+    alu->in1 = in1;
+    alu->in2 = in2;
+    step();
+    EXPECT_EQ(((uint32_t)alu->out) & 1, expected);
+  }
+}
