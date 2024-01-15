2520d78d3c0450a696bdb67b166bba5c7c330c5a
Ryang Sohn <loop.infinitely@gmail.com>
2024-01-15 16:19:00 +0900

    test(decode): add unit test for AUIPC

--- test/decode.cc
+++ test/decode.cc
@@ -21,3 +21,12 @@ TEST_F(DecodeTest, LUI) {
   EXPECT_EQ((uint32_t)component->rd, 1);
   EXPECT_EQ((uint32_t)component->imm, 0x0dead000);
 }
+
+TEST_F(DecodeTest, AUIPC) {
+  component->inst = 0x12345117; // auipc x2, 0x12345
+  step();
+  EXPECT_TRUE((uint32_t)component->valid);
+  EXPECT_EQ((uint32_t)component->opcode, 0b0010111);
+  EXPECT_EQ((uint32_t)component->rd, 2);
+  EXPECT_EQ((uint32_t)component->imm, 0x12345000);
+}
